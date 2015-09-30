d6c4f9516316a2f69189eddf6ff4d14415027f7c
Martijn van Maasakkers <martijn@vanmaasakkers.net>
2015-09-30 09:54:48 +0200

    Changed namespace to Camelcase

--- src/Nicci/Profile.php
+++ src/Nicci/Profile.php
@@ -7,6 +7,27 @@ class Profile {
   private $token = "";
   private $host = "https://profile.nicci.io";
 
+  private $client_id = "";
+  private $client_secret = "";
+  private $client_password = "";
+  private $scope = array("basic");
+  private $state = "";
+  private $redirect_uri = "";
+  private $locale = "en_US";
+
+  public function __construct($config = array()) {
+    $this->readConfig($config);
+  }
+
+  private function readConfig($config = array()) {
+    $vars = array("host", "client_id", "client_secret", "redirect_uri", "scope", "locale");
+		foreach ($vars as $var) {
+			if (isset($config[$var])) {
+				$this->{$var} = $config[$var];
+			}
+		}
+  }
+
   public function setHost($host) {
     $this->host = $host;
   }
